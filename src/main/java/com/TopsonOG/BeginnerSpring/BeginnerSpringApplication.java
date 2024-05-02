89562658a542a273f5687c84f92bc4b4bd62c70d
GustavoPL <gustavopillco01@gmail.com>
2024-05-02 15:49:07 -0400

    Creating the repository

---
+++ src/main/java/com/TopsonOG/BeginnerSpring/BeginnerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.TopsonOG.BeginnerSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeginnerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeginnerSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TopsonOG/BeginnerSpring/BeginnerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TopsonOG.BeginnerSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeginnerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
